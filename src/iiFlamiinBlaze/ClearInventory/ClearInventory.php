b9dedf4659f9a1d0ccf1274e8eb64ae652725c12
808DevTeam <38928140+808DevTeam@users.noreply.github.com>
2018-05-02 20:49:09 -0400

    Update ClearInventory.php

--- src/iiFlamiinBlaze/ClearInventory/ClearInventory.php
+++ src/iiFlamiinBlaze/ClearInventory/ClearInventory.php
@@ -33,32 +33,32 @@ class ClearInventory extends PluginBase{
     const PREFIX = TextFormat::AQUA . "ClearInv" . TextFormat::GOLD . " > ";
 
     public function onEnable() : void{
-        $this->getLogger()->info("ClearInv " . self::VERSION . "by iiFlamiinBlaze is enabled");
+        $this->getLogger()->info("ClearInv " . self::VERSION . "By iiFlamiinBlaze Is Enabled");
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
         if($command->getName() === "clearinv"){
             if(!$sender instanceof Player){
-                $sender->sendMessage(self::PREFIX . TextFormat::RED . "Use this command in-game");
+                $sender->sendMessage(self::PREFIX . TextFormat::RED . "Use This Command In-Game");
                 return true;
             }
             if(!$sender->hasPermission("clearinv.command")){
-                $sender->sendMessage(self::PREFIX . TextFormat::RED . "You do not have permission to use this command");
+                $sender->sendMessage(self::PREFIX . TextFormat::RED . "You Do Not Have Permission To Use This Command");
                 return true;
             }
             if(empty($args[0])){
                 $sender->getInventory()->clearAll();
-                $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "Your inventory has been cleared");
+                $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "Your Inventory Has Been Cleared");
                 return true;
             }
             if($this->getServer()->getPlayer($args[1])){
                 $player = $this->getServer()->getPlayer($args[1]);
                 $name = $player->getName();
                 $player->getInventory()->clearAll();
-                $player->sendMessage(self::PREFIX . TextFormat::GREEN . "Your inventory has been cleared");
-                $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You have cleared $name's inventory");
+                $player->sendMessage(self::PREFIX . TextFormat::GREEN . "Your Inventory has Been Cleared");
+                $sender->sendMessage(self::PREFIX . TextFormat::GREEN . "You Have Cleared $name's Inventory");
             }else{
-                $sender->sendMessage(self::PREFIX . TextFormat::RED . "Player not found");
+                $sender->sendMessage(self::PREFIX . TextFormat::RED . "Player Not Found");
                 return true;
             }
         }
